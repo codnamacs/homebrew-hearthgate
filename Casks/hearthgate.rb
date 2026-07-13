537b1ac99dfb69c983d69a254fc782247dd8dff9
Murat Ozturk <beour19@gmail.com>
2026-07-13 16:33:00 +0300

    Update HearthGate to 1.14.0

--- Casks/hearthgate.rb
+++ Casks/hearthgate.rb
@@ -1,6 +1,6 @@
 cask "hearthgate" do
-  version "1.13.0"
-  sha256 "8b0551b85ae41d6da3a66b81929f827ed1de8b3b6fa3309f6b2ce157894bf7e9"
+  version "1.14.0"
+  sha256 "9a82558810cbe618b28d7704d6993dfa4ee4e811c79a42b17d638a32a9550ca4"
 
   url "https://updates.codnamacs.com/HearthGate-#{version}.dmg",
       verified: "updates.codnamacs.com/"
